48959018e328bfe7d9fe0bdbd141ce77a3a94452
SARBJOT2084 <sarbjot2084.be20@chitkara.edu.in>
2024-06-05 01:22:52 +0530

    Added fixes

---
+++ src/Components/InputField.jsx
@@ -0,0 +1,34 @@
+// InputField.js
+import React from 'react';
+const inputFields = [
+  { label: "First Name", placeholder: "Sagar" },
+  { label: "Last Name", placeholder: "Sharma" },
+  { label: "Address", placeholder: "#434, 5th street" },
+  { label: "City", placeholder: "New York" },
+  { label: "State", placeholder: "Maharashtra" },
+  { label: "Postal Code", placeholder: "411014" }
+];
+
+const Input = ({ label, placeholder }) => {
+  return (
+    <div>
+      <div className="text-xs text-[#35295D] font-bold">{label}</div>
+      <input className="input-box" placeholder={placeholder} />
+    </div>
+  );
+};
+const InputField = () => 
+  {
+    return (
+      <div className="grid grid-cols-2 gap-2 p-2 m-2">
+        {/* Map over inputFields array and render each input field */}
+        {inputFields.map((field, index) => (
+          <Input index={index} 
+            label={field.label}
+            placeholder={field.placeholder}
+          />
+        ))}
+      </div>
+    );
+  }
+export default InputField;
--- src/Components/PaymentMethods.jsx
+++ src/Components/PaymentMethods.jsx
@@ -1,23 +1,26 @@
 import React from 'react'
 
-const images=[{
-    src:"https://i.pinimg.com/originals/38/2f/0a/382f0a8cbcec2f9d791702ef4b151443.png"
-},{
-    src:"https://upload.wikimedia.org/wikipedia/commons/c/cb/Rupay-Logo.png"
-},{
-    src:"https://cdn4.iconfinder.com/data/icons/flat-brand-logo-2/512/visa-512.png"
-},{
-    src:"https://logowik.com/content/uploads/images/upi-unified-payments-interface4949.logowik.com.webp"
-}]
+const images=["https://i.pinimg.com/originals/38/2f/0a/382f0a8cbcec2f9d791702ef4b151443.png",
+"https://upload.wikimedia.org/wikipedia/commons/c/cb/Rupay-Logo.png"
+,"https://cdn4.iconfinder.com/data/icons/flat-brand-logo-2/512/visa-512.png"
+,"https://cdn.icon-icons.com/icons2/2699/PNG/512/upi_logo_icon_170312.png"
+]
 
-const PaymentMethods = ({img}) => {
-  return (
-    <div className="bg-gray-200 w-20 flex items-center justify-center">
-        <img 
-        src={img.src}
-       height={30} width={30} alt="Visa" className="mx-auto my-auto" />
-    </div>
-  )
+const PaymentMethods = () => {
+    return (
+        <div className="grid grid-cols-5 gap-2 p-2 m-2">
+            {images.map((imageUrl, index) => (
+                <button className="bg-gray-200 w-20 flex items-center justify-center">        
+                    <img key={index} src={imageUrl} 
+                    height={30} 
+                    width={30} 
+                    alt={`Payment Method ${index + 1}`} />
+                
+                </button>
+            ))}
+        </div>
+        )
 }
+
 
 export default PaymentMethods
